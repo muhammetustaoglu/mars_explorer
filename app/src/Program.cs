ea57886a78fa17cc28b0f737681768f0abd14493
Muhammet USTAOGLU <muhammet.ustaoglu@edenred.com>
2021-12-27 17:05:28 +0300

    first commit

---
+++ app/src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mars_explorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
